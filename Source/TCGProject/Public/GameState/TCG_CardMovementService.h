4527890a642d7beeb82fdfb0ff50144234dcafb6
Lolor92 <89623320+Lolor92@users.noreply.github.com>
2026-06-24 11:38:17 -0700

    Declare remaining card movement helpers in service

--- Source/TCGProject/Public/GameState/TCG_CardMovementService.h
+++ Source/TCGProject/Public/GameState/TCG_CardMovementService.h
@@ -15,4 +15,8 @@ class TCGPROJECT_API UTCG_CardMovementService : public UObject
 public:
 	static bool MoveCardToLocation(ATCG_GameState* GameState, const FGuid& CardInstanceId, ETCGCardLocation NewLocation);
 	static bool MoveStackToLocation(ATCG_GameState* GameState, const FGuid& StackId, ETCGCardLocation NewLocation);
+	static bool MoveBottomOverlayToGraveyard(ATCG_GameState* GameState, const FGuid& TargetCardInstanceId);
+	static bool PlayGraveyardCardToEmptyZone(ATCG_GameState* GameState, const FGuid& CardInstanceId, FName ZoneId);
+	static bool ReturnUnitStackToBottomDeckMaterialsToGraveyard(ATCG_GameState* GameState, const FGuid& TargetTopCardInstanceId);
+	static bool ResolveAttackMillTwoWaterBounceBattlingUnit(ATCG_GameState* GameState, const FGuid& SourceCardInstanceId, const FGuid& BattleTargetCardInstanceId);
 };
