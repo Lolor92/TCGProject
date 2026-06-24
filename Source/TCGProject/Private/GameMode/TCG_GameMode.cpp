ff8595838ed1cba33ddc99d558798d60193d9a83
Lolor92 <89623320+Lolor92@users.noreply.github.com>
2026-06-24 07:37:40 -0700

    Run water graveyard round bonus scenario

--- Source/TCGProject/Private/GameMode/TCG_GameMode.cpp
+++ Source/TCGProject/Private/GameMode/TCG_GameMode.cpp
@@ -30,8 +30,8 @@ void ATCG_GameMode::PostLogin(APlayerController* NewPlayer)
 		ATCG_GameState* TCGGameState = GetGameState<ATCG_GameState>();
 		if (TCGGameState)
 		{
-			// Dedicated attack mill, attach Water material debug scenario.
-			TCGGameState->RunDebugAttackMillAttachScenario();
+			// Dedicated Water graveyard round bonus debug scenario.
+			TCGGameState->RunDebugWaterGraveyardBoostScenario();
 		}
 	}
 }
