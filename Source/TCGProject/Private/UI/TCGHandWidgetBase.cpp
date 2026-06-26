ff241d3725d4db1c0dfe00442f76aef92d0c3f2d
Lolor92 <89623320+Lolor92@users.noreply.github.com>
2026-06-26 04:22:46 -0700

    Reuse hand card widgets on HUD refresh

--- Source/TCGProject/Private/UI/TCGHandWidgetBase.cpp
+++ Source/TCGProject/Private/UI/TCGHandWidgetBase.cpp
@@ -3,119 +3,170 @@
 #include "Components/PanelWidget.h"
 #include "UI/TCGCardWidgetBase.h"
 
-void UTCGHandWidgetBase::SetHandData(const FTCGHandWidgetData& InHandData)
+namespace
 {
-HandData = InHandData;
+	bool DoesHandWidgetLayoutMatch(
+		const TArray<TObjectPtr<UTCGCardWidgetBase>>& SpawnedCardWidgets,
+		const FTCGHandWidgetData& NewHandData)
+	{
+		if (SpawnedCardWidgets.Num() != NewHandData.Cards.Num())
+		{
+			return false;
+		}
 
-if (bAutoRebuildCardWidgets)
-{
-RebuildHandCards();
+		for (int32 CardIndex = 0; CardIndex < NewHandData.Cards.Num(); ++CardIndex)
+		{
+			const UTCGCardWidgetBase* ExistingCardWidget = SpawnedCardWidgets[CardIndex];
+			if (!ExistingCardWidget)
+			{
+				return false;
+			}
+
+			const FTCGCardWidgetData& ExistingCardData = ExistingCardWidget->GetCardData();
+			const FTCGCardWidgetData& NewCardData = NewHandData.Cards[CardIndex];
+
+			if (ExistingCardData.HandIndex != NewCardData.HandIndex
+				|| ExistingCardData.SourceObject != NewCardData.SourceObject)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
 
-BP_OnHandDataChanged();
-BP_OnHandSelectionChanged(HandData.SelectedHandIndex);
+void UTCGHandWidgetBase::SetHandData(const FTCGHandWidgetData& InHandData)
+{
+	const bool bCanReuseCardWidgets = bAutoRebuildCardWidgets
+		&& DoesHandWidgetLayoutMatch(SpawnedCardWidgets, InHandData);
+
+	HandData = InHandData;
+
+	if (bCanReuseCardWidgets)
+	{
+		for (int32 CardIndex = 0; CardIndex < HandData.Cards.Num(); ++CardIndex)
+		{
+			UTCGCardWidgetBase* CardWidget = SpawnedCardWidgets[CardIndex];
+			if (!CardWidget)
+			{
+				continue;
+			}
+
+			const FTCGCardWidgetData& CardData = HandData.Cards[CardIndex];
+			CardWidget->SetCardData(CardData);
+			CardWidget->SetSelected(CardData.HandIndex == HandData.SelectedHandIndex);
+		}
+	}
+	else if (bAutoRebuildCardWidgets)
+	{
+		RebuildHandCards();
+	}
+
+	BP_OnHandDataChanged();
+	BP_OnHandSelectionChanged(HandData.SelectedHandIndex);
 }
 
 void UTCGHandWidgetBase::SetCardWidgetClass(TSubclassOf<UTCGCardWidgetBase> InCardWidgetClass)
 {
-CardWidgetClass = InCardWidgetClass;
+	CardWidgetClass = InCardWidgetClass;
 
-if (bAutoRebuildCardWidgets)
-{
-RebuildHandCards();
-}
+	if (bAutoRebuildCardWidgets)
+	{
+		RebuildHandCards();
+	}
 }
 
 void UTCGHandWidgetBase::RebuildHandCards()
 {
-SpawnedCardWidgets.Reset();
+	SpawnedCardWidgets.Reset();
 
-if (!CardContainer)
-{
-return;
-}
+	if (!CardContainer)
+	{
+		return;
+	}
 
-CardContainer->ClearChildren();
+	CardContainer->ClearChildren();
 
-if (!CardWidgetClass)
-{
-return;
-}
+	if (!CardWidgetClass)
+	{
+		return;
+	}
 
-for (const FTCGCardWidgetData& CardData : HandData.Cards)
-{
-UTCGCardWidgetBase* CardWidget = CreateWidget<UTCGCardWidgetBase>(GetOwningPlayer(), CardWidgetClass);
-if (!CardWidget)
-{
-continue;
-}
+	for (const FTCGCardWidgetData& CardData : HandData.Cards)
+	{
+		UTCGCardWidgetBase* CardWidget = CreateWidget<UTCGCardWidgetBase>(GetOwningPlayer(), CardWidgetClass);
+		if (!CardWidget)
+		{
+			continue;
+		}
 
-CardWidget->SetCardData(CardData);
-CardWidget->SetSelected(CardData.HandIndex == HandData.SelectedHandIndex);
-CardWidget->OnCardClicked.AddUniqueDynamic(this, &UTCGHandWidgetBase::HandleCardWidgetClicked);
+		CardWidget->SetCardData(CardData);
+		CardWidget->SetSelected(CardData.HandIndex == HandData.SelectedHandIndex);
+		CardWidget->OnCardClicked.AddUniqueDynamic(this, &UTCGHandWidgetBase::HandleCardWidgetClicked);
 
-CardContainer->AddChild(CardWidget);
-SpawnedCardWidgets.Add(CardWidget);
-}
+		CardContainer->AddChild(CardWidget);
+		SpawnedCardWidgets.Add(CardWidget);
+	}
 }
 
 void UTCGHandWidgetBase::SelectHandCard(const int32 HandIndex)
 {
-FTCGCardWidgetData SelectedCardData;
-if (!GetCardDataByHandIndex(HandIndex, SelectedCardData))
-{
-return;
-}
+	FTCGCardWidgetData SelectedCardData;
+	if (!GetCardDataByHandIndex(HandIndex, SelectedCardData))
+	{
+		return;
+	}
 
-HandData.SelectedHandIndex = HandIndex;
+	HandData.SelectedHandIndex = HandIndex;
 
-for (UTCGCardWidgetBase* CardWidget : SpawnedCardWidgets)
-{
-if (CardWidget)
-{
-CardWidget->SetSelected(CardWidget->GetCardData().HandIndex == HandData.SelectedHandIndex);
-}
-}
+	for (UTCGCardWidgetBase* CardWidget : SpawnedCardWidgets)
+	{
+		if (CardWidget)
+		{
+			CardWidget->SetSelected(CardWidget->GetCardData().HandIndex == HandData.SelectedHandIndex);
+		}
+	}
 
-OnHandCardSelected.Broadcast(HandIndex, SelectedCardData.SourceObject);
-BP_OnHandSelectionChanged(HandData.SelectedHandIndex);
+	OnHandCardSelected.Broadcast(HandIndex, SelectedCardData.SourceObject);
+	BP_OnHandSelectionChanged(HandData.SelectedHandIndex);
 }
 
 void UTCGHandWidgetBase::ClearHandSelection()
 {
-if (HandData.SelectedHandIndex == INDEX_NONE)
-{
-return;
-}
+	if (HandData.SelectedHandIndex == INDEX_NONE)
+	{
+		return;
+	}
 
-HandData.SelectedHandIndex = INDEX_NONE;
+	HandData.SelectedHandIndex = INDEX_NONE;
 
-for (UTCGCardWidgetBase* CardWidget : SpawnedCardWidgets)
-{
-if (CardWidget)
-{
-CardWidget->SetSelected(false);
-}
-}
+	for (UTCGCardWidgetBase* CardWidget : SpawnedCardWidgets)
+	{
+		if (CardWidget)
+		{
+			CardWidget->SetSelected(false);
+		}
+	}
 
-BP_OnHandSelectionChanged(HandData.SelectedHandIndex);
+	BP_OnHandSelectionChanged(HandData.SelectedHandIndex);
 }
 
 void UTCGHandWidgetBase::HandleCardWidgetClicked(const int32 HandIndex, UObject* SourceObject)
 {
-SelectHandCard(HandIndex);
+	SelectHandCard(HandIndex);
 }
 
 bool UTCGHandWidgetBase::GetCardDataByHandIndex(const int32 HandIndex, FTCGCardWidgetData& OutCardData) const
 {
-for (const FTCGCardWidgetData& CardData : HandData.Cards)
-{
-if (CardData.HandIndex == HandIndex)
-{
-OutCardData = CardData;
-return true;
-}
-}
+	for (const FTCGCardWidgetData& CardData : HandData.Cards)
+	{
+		if (CardData.HandIndex == HandIndex)
+		{
+			OutCardData = CardData;
+			return true;
+		}
+	}
 
-return false;
+	return false;
 }
